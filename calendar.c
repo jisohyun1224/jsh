b635248803d4a80332ebb30be3b7cf5ff73f5de7
jisohyun1224 <ys03074@naver.com>
2017-05-01 16:38:31 +0900

    add test.txt

--- calendar.c
+++
@@ -1,7 +0,0 @@
-#include "diary.h"
-//this is calendar
-int calendar()
-{
-	printf("function calendar.\n");
-	return 0;
-}
--- diary.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-//this is calendar();
-int memo();
-int calendar();
